de8709c03974a4c97c553d091c5656ce1192e3cf
bila <User@zizas.0836323464>
2026-05-16 23:37:11 +0200

    fix: volta ao render

--- src/api.js
+++ src/api.js
@@ -1,9 +1,6 @@
 ﻿import axios from "axios";
 const api = axios.create({
-    baseURL: "https://swinging-proofing-vocation.ngrok-free.dev/api",
-    headers: {
-        "ngrok-skip-browser-warning": "true"
-    }
+    baseURL: "https://assistia-kmbl.onrender.com/api",
 });
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");

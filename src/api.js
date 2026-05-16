176f4cfdc849bec3bbf7ac967456919b4ce86b30
bila <User@zizas.0836323464>
2026-05-16 22:34:18 +0200

    fix: api url ngrok

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 ﻿import axios from "axios";
-
 const api = axios.create({
-    baseURL: "https://assistia-kmbl.onrender.com/api",
+    baseURL: "https://swinging-proofing-vocation.ngrok-free.dev/api",
+    headers: {
+        "ngrok-skip-browser-warning": "true"
+    }
 });
-
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -11,17 +12,4 @@ api.interceptors.request.use((config) => {
     }
     return config;
 });
-
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response && error.response.status === 401) {
-            localStorage.removeItem("token");
-            localStorage.removeItem("user");
-            window.location.href = "/login";
-        }
-        return Promise.reject(error);
-    }
-);
-
 export default api;

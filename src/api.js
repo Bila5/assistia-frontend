535a19a79898087330e45911e0e3f81432f620d6
bila <User@zizas.0836323464>
2026-05-16 15:42:18 +0200

    Fix Dashboard map error

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 ﻿import axios from "axios";
+
 const api = axios.create({
     baseURL: "https://assistia-kmbl.onrender.com/api",
 });
+
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -9,4 +11,17 @@ api.interceptors.request.use((config) => {
     }
     return config;
 });
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+            window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default api;

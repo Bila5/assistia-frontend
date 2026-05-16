34eba59098a7ff396de61d7c3577fb02e1cb8574
bila <User@zizas.0836323464>
2026-05-16 12:27:35 +0200

    fix: api url para render

--- src/api.js
+++ src/api.js
@@ -1,16 +1,12 @@
-import axios from 'axios';
-
+﻿import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
-    
+    baseURL: "https://assistia-kmbl.onrender.com/api",
 });
-
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
 });
-
 export default api;

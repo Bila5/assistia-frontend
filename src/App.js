b19a2cced346950d531184666f3429bb74ce2411
bila <User@zizas.0836323464>
2026-05-16 09:58:45 +0200

    fix: App.js organization route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import Conversations from './pages/Conversations';
-import Organization from './pages/Organization';
-import ConversationShow from './pages/ConversationShow';
+﻿import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Conversations from "./pages/Conversations";
+import Organization from "./pages/Organization";
+import ConversationShow from "./pages/ConversationShow";
 
 function PrivateRoute({ children }) {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   return token ? children : <Navigate to="/login" />;
 }
 
@@ -20,7 +20,8 @@ function App() {
         <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
         <Route path="/conversations" element={<PrivateRoute><Conversations /></PrivateRoute>} />
         <Route path="/conversations/:id" element={<PrivateRoute><ConversationShow /></PrivateRoute>} />
-        <Route path="/organization" element={<PrivateRoute><Organization /></PrivateRoute>} />`n        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="/organization" element={<PrivateRoute><Organization /></PrivateRoute>} />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
